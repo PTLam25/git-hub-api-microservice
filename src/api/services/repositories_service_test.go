263d66630f3777247b47d2b6e70df77b5ea1ce86
Pham Tung Lam <lam.famtung@gmail.com>
2021-05-11 15:16:36 +0300

    created tests for CreateRepos service function

--- src/api/services/repositories_service_test.go
+++ src/api/services/repositories_service_test.go
@@ -3,11 +3,13 @@ package services
 import (
 	"github.com/PTLam25/git-hub-api-microservice/src/api/client/restclient"
 	"github.com/PTLam25/git-hub-api-microservice/src/api/domain/repositories"
+	"github.com/PTLam25/git-hub-api-microservice/src/api/utils/errors"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -157,4 +159,131 @@ func TestCreateRepoConcurrentNoError(t *testing.T) {
 	assert.EqualValues(t, 123, result.Response.Id)
 	assert.EqualValues(t, "", result.Response.Name)
 	assert.EqualValues(t, "", result.Response.Owner)
+}
+
+func TestHandleRepoResults(t *testing.T) {
+	// 1) инициализация
+	input := make(chan repositories.CreateRepositoriesResult)
+	output := make(chan repositories.CreateReposResponse)
+	defer close(output)
+	var wg sync.WaitGroup
+	service := repoService{}
+
+	// 2) вызов функции для теста
+	go service.handleRepoResults(&wg, input, output)
+	wg.Add(1)
+	go func() {
+		input <- repositories.CreateRepositoriesResult{
+			Error: errors.NewBadRequestError("invalid repository name"),
+		}
+	}()
+
+	wg.Wait()
+	close(input)
+	result := <-output
+
+	// 3) валидация
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 0, result.StatusCode)
+	assert.EqualValues(t, 1, len(result.Results))
+
+	assert.NotNil(t, result.Results[0].Error)
+	assert.EqualValues(t, http.StatusBadRequest, result.Results[0].Error.Status())
+	assert.EqualValues(t, "invalid repository name", result.Results[0].Error.Message())
+}
+
+func TestCreateReposInvalidRequest(t *testing.T) {
+	// 1) инициализация
+	requests := []repositories.CreateRepoRequest{
+		{},
+		{Name: "    "},
+	}
+
+	// 2) вызов функции для теста
+	result, err := RepositoryService.CreateRepos(requests)
+
+	// 3) валидация
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 2, len(result.Results))
+	assert.EqualValues(t, http.StatusBadRequest, result.StatusCode)
+
+	assert.EqualValues(t, http.StatusBadRequest, result.Results[0].Error.Status())
+	assert.EqualValues(t, "invalid repository name", result.Results[0].Error.Message())
+
+	assert.EqualValues(t, http.StatusBadRequest, result.Results[1].Error.Status())
+	assert.EqualValues(t, "invalid repository name", result.Results[1].Error.Message())
+}
+
+func TestCreateReposOneSuccessOneFail(t *testing.T) {
+	// 1) инициализация
+	restclient.FlushMockUps()
+	restclient.AddMockUp(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": 123}`)),
+		},
+	})
+
+	requests := []repositories.CreateRepoRequest{
+		{},
+		{Name: "testing"},
+	}
+
+	// 2) вызов функции для теста
+	result, err := RepositoryService.CreateRepos(requests)
+
+	// 3) валидация
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 2, len(result.Results))
+	assert.EqualValues(t, http.StatusPartialContent, result.StatusCode)
+
+	for _, result := range result.Results {
+		if result.Error != nil {
+			assert.EqualValues(t, http.StatusBadRequest, result.Error.Status())
+			assert.EqualValues(t, "invalid repository name", result.Error.Message())
+			continue
+		}
+
+		assert.EqualValues(t, 123, result.Response.Id)
+		assert.EqualValues(t, "", result.Response.Name)
+		assert.EqualValues(t, "", result.Response.Owner)
+	}
+}
+
+func TestCreateReposAllSuccess(t *testing.T) {
+	// 1) инициализация
+	restclient.FlushMockUps()
+	restclient.AddMockUp(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": 123}`)),
+		},
+	})
+
+	requests := []repositories.CreateRepoRequest{
+		{Name: "testing"},
+		{Name: "testing"},
+	}
+
+	// 2) вызов функции для теста
+	result, err := RepositoryService.CreateRepos(requests)
+
+	// 3) валидация
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 2, len(result.Results))
+	assert.EqualValues(t, http.StatusCreated, result.StatusCode)
+
+	for _, result := range result.Results {
+		assert.Nil(t, result.Error)
+		assert.EqualValues(t, 123, result.Response.Id)
+		assert.EqualValues(t, "", result.Response.Name)
+		assert.EqualValues(t, "", result.Response.Owner)
+	}
 }
